86093079a0fcbe15e91b3223aedf5ea05d4dd121
daynaaaa <danya.eggy@gmail.com>
2024-12-23 02:12:24 -0500

    initial commit

---
+++ app/dashboard/page.js
@@ -0,0 +1,15 @@
+import Main from "@/components/Main";
+
+export const metadata = {
+    title: "Tracky | Dashboard",
+  };
+
+export default function DashboardPage(){
+    const isAuthenticated = false
+    
+    return (
+        <Main>
+
+        </Main>
+    )
+}
